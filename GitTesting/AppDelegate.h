4ac6d2e30c37f70ef25990b5bc5e199600e2ae1d
ajayparmar811 <parmarajay811@gmail.com>
2015-01-13 23:34:09 +0530

    Initial Commit

---
+++ GitTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTesting
+//
+//  Created by Ajay on 13/01/15.
+//  Copyright (c) 2015 Ajay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTesting/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTesting
+//
+//  Created by Ajay on 13/01/15.
+//  Copyright (c) 2015 Ajay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
